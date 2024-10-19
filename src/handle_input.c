044e6ddefda28ef7aaf1445c4e40a04237f5b085
Renata2202 <renata_ag@hotmail.com>
2024-10-19 19:00:22 +0100

    testes

--- src/handle_input.c
+++ src/handle_input.c
@@ -6,7 +6,7 @@
 /*   By: rnunes-a <rnunes-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 15:39:38 by rnunes-a          #+#    #+#             */
-/*   Updated: 2024/10/19 18:27:47 by rnunes-a         ###   ########.fr       */
+/*   Updated: 2024/10/19 18:43:28 by rnunes-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,60 @@ int check_extension(char *file)
     return (ft_strncmp(file_extension, ber, 4));
 }
 
-
-int	check_wall(char *line)
+int check_first_last_line(char *line)
 {
-	int	i;
-
-	i = 0;
-	while (line[i] && line[i] != '\n')
-	{
-		if (line[i] != '1')
-			return (1);
-		i++;
-	}
-	return (0);
+    if (*line == '\0')
+        return 0;
+    if (*line != '1')
+        return 1;
+    return check_first_last_line(line + 1);
 }
+
+int check_wall_columns(char **map, int row, int total_rows)
+{
+    if (row >= total_rows - 1) // Condição de parada para as linhas intermediárias
+        return 0; // Retorna 0 se todas as verificações forem bem-sucedidas
+
+    // Verifica as colunas da linha atual
+    if (map[row][0] != '1' || map[row][strlen(map[row]) - 1] != '1')
+        return 1; // Retorna 1 se as colunas não forem '1's
+
+    return check_wall_columns(map, row + 1, total_rows); // Chama a função recursivamente para a próxima linha
+}
+
+int check_map_walls(char **map, int rows)
+{
+    if (rows < 3)
+        return 1; // Retorna 1 se o mapa não tiver paredes suficientes
+
+    // Verifica a primeira linha
+    if (check_first_last_line(map[0]) != 0)
+        return 1; // Retorna 1 se a primeira linha não estiver correta
+
+    // Verifica a última linha
+    if (check_first_last_line(map[rows - 1]) != 0)
+        return 1; // Retorna 1 se a última linha não estiver correta
+
+    // Verifica as colunas da primeira e última linha para as linhas intermediárias
+    if (check_wall_columns(map, 1, rows) != 0)
+        return 1; // Retorna 1 se as colunas das linhas intermediárias não estiverem corretas
+
+    return 0; // Retorna 0 se todas as verificações forem bem-sucedidas
+}
+
+// int	check_wall(char *line)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while (line[i] && line[i] != '\n')
+// 	{
+// 		if (line[i] != '1')
+// 			return (1);
+// 		i++;
+// 	}
+// 	return (0);
+// }
 
 int	check_map_content(char *line, t_map *map, int n_line)
 {
