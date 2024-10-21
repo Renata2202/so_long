97a807d4a65a0089be5fff5f4cf6cd0a60c7eff8
Renata2202 <renata_ag@hotmail.com>
2024-10-21 18:58:18 +0100

    main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rnunes-a <rnunes-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 15:35:25 by rnunes-a          #+#    #+#             */
-/*   Updated: 2024/10/21 17:18:51 by rnunes-a         ###   ########.fr       */
+/*   Updated: 2024/10/21 18:20:24 by rnunes-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,12 @@
 int main(int argc, char **argv)
 {
 	t_data *data;
-
+	// Verifica a extensão do arquivo
+	if (check_extension(argv[1]))
+	{
+		printf("Erro: Extensão do arquivo inválida. Use arquivos .ber.\n");
+		return (1);
+	}
 	data = initialize_data();
 	if (!data)
 		return (1);
@@ -23,7 +28,7 @@ int main(int argc, char **argv)
 	// Supondo que o map_data já foi preenchido com o mapa e suas dimensões
 	if (check_map_shape(data->map) || check_map_walls_and_chars(data->map))
 	{
-		ft_printf("Erro: Mapa inválido.\n");
+		printf("Erro: Mapa inválido.\n");
 		free(data->textures);
 		free(data->map);
 		free(data);
@@ -32,10 +37,34 @@ int main(int argc, char **argv)
 
 	// Código para o loop principal do jogo aqui (game loop, etc.)
 
+	// Libera memória antes de sair
 	free(data->textures);
 	free(data->map);
 	free(data);
 	return (0);
+}
+
+int load_map(char *filename, t_map *map)
+{
+    int     fd;
+    char    *line;
+    int     row_count;
+
+    fd = open(filename, O_RDONLY);
+    if (fd < 0)
+        return (1); // Erro ao abrir o arquivo
+
+    row_count = 0;
+    while (get_next_line(fd, &line) > 0)
+    {
+        // Alocar espaço e armazenar cada linha do mapa
+        map->map[row_count] = ft_strdup(line);
+        row_count++;
+        free(line);
+    }
+    map->rows = row_count;
+    close(fd);
+    return (0);
 }
 
 
